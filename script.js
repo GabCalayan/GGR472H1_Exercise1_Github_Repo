ee8b301649edae03b234db8cf3481c20c0ae35bf
GabCalayan <95249972+GabCalayan@users.noreply.github.com>
2023-01-20 22:37:01 -0500

    Added new functionality and looks to index.html and script.js
    Basically added new code so that the website looks better and added new functions in script.js for a new button in the html file

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function clicked() {
 }
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 //Clicking function 
+
+//Event listener function 
+var button = document.getElementById("my-btn");
+    button.addEventListener("click", function() {
+        alert("You clicked me!");
+    });
